9a85c561e4be37630e9535930e509ae2f11af7d9
stuart <sdwood@gmail.com>
2022-12-01 10:41:09 +0000

    day 1 part 2 solution

--- day1/src/main/clojure/day_1.clj
+++ day1/src/main/clojure/day_1.clj
@@ -2,20 +2,32 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn most-calories-carried-by-an-elf [input]
-  (let [greeting "Hello, World!!!"]
-    (->> input
-         io/resource
-        slurp
-         string/split-lines
-         (map #(when-not (string/blank? %)
-                 (Long/parseLong %)))
-         (partition-by nil?)
-         (remove #(every? nil? %))
-         (map #(apply + %))
-         (apply max)
-        )
-    ))
+(defn- read-and-parse-input [input]
+  (->> input
+       io/resource
+       slurp
+       string/split-lines
+       (map #(when-not (string/blank? %)
+               (Long/parseLong %)))
+       (partition-by nil?)
+       (remove #(every? nil? %))
+       (map #(apply + %))))
+
+(defn most-calories-carried-by-an-elf
+  "day 1 part 1"
+  [input]
+  (->> input
+       read-and-parse-input
+       (apply max)))
+
+(defn calories-carried-by-the-top-3-elves
+  "day 1 part 2"
+  [input]
+  (->> input
+       read-and-parse-input
+       (sort >)
+       (take 3)
+       (apply +)))
 
 
 
--- day1/src/test/clojure/day_1_test.clj
+++ day1/src/test/clojure/day_1_test.clj
@@ -4,3 +4,6 @@
 
 (deftest day-1-part-1-test
   (is (= 24000 (most-calories-carried-by-an-elf "test-input"))))
+
+(deftest day-1-part-2-test
+  (is (= 45000 (calories-carried-by-the-top-3-elves "test-input"))))
