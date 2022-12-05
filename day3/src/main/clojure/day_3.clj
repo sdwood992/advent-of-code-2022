6888a943b0a4251caf65c565b24238900138bbca
stuart <sdwood@gmail.com>
2022-12-05 09:41:43 +0000

    initial day 3 module

---
+++ day3/src/main/clojure/day_3.clj
@@ -0,0 +1,10 @@
+(ns day-3)
+
+(defn blah []
+  (let [greeting "Hello, World!!!"]
+    (println greeting)
+    greeting))
+
+
+
+
---
+++ day3/src/test/clojure/day_3_test.clj
@@ -0,0 +1,6 @@
+(ns day-3-test
+  (:require [clojure.test :refer :all]
+            [day-3 :refer :all]))
+
+(deftest simple-test
+  (is (= "Hello, World!!!" (blah))))
