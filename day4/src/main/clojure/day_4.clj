9b7f62acfde669a746154e62a9d963e1acb15eff
stuart <sdwood@gmail.com>
2022-12-05 17:44:51 +0000

    initial day 4 structure

---
+++ day4/src/main/clojure/day_4.clj
@@ -0,0 +1,12 @@
+(ns day-4
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn blah []
+  (let [greeting "Hello, World!!!"]
+    (println greeting)
+    greeting))
+
+
+
+
---
+++ day4/src/test/clojure/day_4_test.clj
@@ -0,0 +1,6 @@
+(ns day-4-test
+  (:require [clojure.test :refer :all]
+            [day-4 :refer :all]))
+
+(deftest simple-test
+  (is (= "Hello, World!!!" (blah))))
