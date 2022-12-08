16e4faeadb0e607b9e8d649e2cc83a868c4fdb23
stuart <sdwood@gmail.com>
2022-12-08 11:51:44 +0000

    Day 6 part 1 solution

--- day6/src/main/clojure/day_6.clj
+++ day6/src/main/clojure/day_6.clj
@@ -2,11 +2,26 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn blah []
-  (let [greeting "Hello, World!!!"]
-    (println greeting)
-    greeting))
+(defn read-input [input]
+  (-> input
+      io/resource
+      slurp))
 
+(defn start-of-packet-marker [data-buffer]
+  (->> data-buffer
+       (partition 4 1)
+       (reduce
+         (fn [acc buffer-segment]
+           (if (= 4 (-> buffer-segment distinct count))
+             (reduced (+ acc 4))
+             (inc acc)))
+         0)))
+
+
+(defn start-of-packet-marker-from-file [input-file]
+  (->> input-file
+       read-input
+       start-of-packet-marker))
 
 
 
--- day6/src/test/clojure/day_6_test.clj
+++ day6/src/test/clojure/day_6_test.clj
@@ -2,5 +2,13 @@
   (:require [clojure.test :refer :all]
             [day-6 :refer :all]))
 
-(deftest simple-test
-  (is (= "Hello, World!!!" (blah))))
+(deftest day-6-part-1-test
+
+  (is (= 7 (start-of-packet-marker "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
+  (is (= 5 (start-of-packet-marker "bvwbjplbgvbhsrlpgdmjqwftvncz")))
+  (is (= 6 (start-of-packet-marker "nppdvjthqldpwncqszvftbrmjlhg")))
+  (is (= 10 (start-of-packet-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
+  (is (= 11 (start-of-packet-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")))
+
+
+  (is (= 7 (start-of-packet-marker-from-file "test-input"))))
