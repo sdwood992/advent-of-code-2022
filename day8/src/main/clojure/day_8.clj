0c78bdd5ce3de46be0e712c233633bbffcf8054d
stuart <sdwood@gmail.com>
2022-12-16 13:14:48 +0000

    Day 8 part 2 solution

--- day8/src/main/clojure/day_8.clj
+++ day8/src/main/clojure/day_8.clj
@@ -46,6 +46,39 @@
          (filter (partial visible? trees))
          count)))
 
+(defn- seen-trees [trees-in-direction tree-in-question]
+  (reduce (fn [acc tree]
+            (if (< tree tree-in-question)
+              (conj acc tree)
+              (reduced (conj acc tree))))
+          []
+          trees-in-direction))
+
+(defn scenic-score [trees [x y]]
+  (let [tree-in-question (tree-at trees x y)
+        row (row trees x)
+        col (col trees y)
+        trees-to-left (take y row)
+        trees-to-right (drop (inc y) row)
+        trees-to-top (take x col)
+        trees-to-bottom (drop (inc x) col)]
+    [[x y] (reduce * (map count [(seen-trees (reverse trees-to-left) tree-in-question)
+                                 (seen-trees trees-to-right tree-in-question)
+                                 (seen-trees (reverse trees-to-top) tree-in-question)
+                                 (seen-trees trees-to-bottom tree-in-question)]))]))
+
+(defn scenic-score-trees [input]
+  (let [trees (read-input input)
+        trees-to-check (for [x (range 0 (-> trees first count))
+                             y (range 0 (-> trees count))]
+                         [x y])]
+    (->> trees-to-check
+         (map (partial scenic-score trees))
+         (into {})
+         (sort-by val >)
+         first
+         val)))
+
 
 
 
--- day8/src/test/clojure/day_8_test.clj
+++ day8/src/test/clojure/day_8_test.clj
@@ -2,5 +2,8 @@
   (:require [clojure.test :refer :all]
             [day-8 :refer :all]))
 
-(deftest simple-test
+(deftest day-8-part-1-test
   (is (= 21 (visible-trees "test-input"))))
+
+(deftest day-8-part-2-test
+  (is (= 8 (scenic-score-trees "test-input"))))
